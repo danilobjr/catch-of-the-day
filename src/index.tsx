8d303645e19de9f6219edda4a7d228c124b640d6
Danilo Barros <danilobjr@gmail.com>
2016-05-20 16:02:37 -0300

    changing app container id

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('main'));
