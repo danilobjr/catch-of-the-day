d9714720ea5d1be3cb25ec699654a98ee8bc36d0
Danilo Barros <danilobjr@gmail.com>
2016-05-21 01:29:52 -0300

    MenuItem component

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import { Header } from './../common';
+import { MenuImage } from './MenuImage';
 
 export class MenuItem extends React.Component<any, any> {
     render() {
         return (
             <li className="menu-item">
-                Menu Item
-                {/* 
-                <figure></figure>
+                {/*
+                <MenuImage url="http://prazeresdamesa.uol.com.br/wordpress/wp-content/uploads/arquivos/imagem_10833.jpg" />
+                */}
                 <Header>Menu Item</Header>
                 <p>Item description</p>
+                {/*
                 <AddToOrderButton />
                 <Price value={2500} />
                 */}
