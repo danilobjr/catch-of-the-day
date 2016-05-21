df460784990406f4ae0d86a8303ae4e16c663fde
Danilo Barros <danilobjr@gmail.com>
2016-05-21 17:00:22 -0300

    not exposing Currency component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,13 +3,11 @@ import { SectionHeader } from './SectionHeader';
 import { Header } from './Header';
 import { Button } from './Button';
 import { Price } from './Price';
-import { Currency } from './Currency';
 
 export {
     Section,
     SectionHeader,
     Header,
     Button,
-    Price,
-    Currency
+    Price
 }
