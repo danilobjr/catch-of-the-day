0645ece40c1df1df4603eba40671de04a56b352d
Danilo Barros <danilobjr@gmail.com>
2016-05-21 12:40:49 -0300

    styling children of MenuItem

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -6,15 +6,17 @@ export class MenuItem extends React.Component<any, any> {
     render() {
         return (
             <li className="menu-item">
-                {/*
-                <MenuImage url="http://prazeresdamesa.uol.com.br/wordpress/wp-content/uploads/arquivos/imagem_10833.jpg" />
-                */}
-                <Header>Menu Item</Header>
-                <p>Item description</p>
-                {/*
-                <AddToOrderButton />
-                <Price value={2500} />
-                */}
+                <div>
+                    <MenuImage url="http://prazeresdamesa.uol.com.br/wordpress/wp-content/uploads/arquivos/imagem_10833.jpg" />
+                </div>
+                <div>
+                    <Header>Menu Item</Header>
+                    <p>Item description</p>
+                    {/*
+                    <AddToOrderButton />
+                    <Price value={2500} />
+                    */}
+                </div>
             </li>
         );
     }
