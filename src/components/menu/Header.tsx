dfc5b98f819ab443c25b2eb6ec32cc2137a7f75f
Danilo Barros <danilobjr@gmail.com>
2016-05-20 23:41:15 -0300

    style for menu header component

--- src/components/menu/Header.tsx
+++ src/components/menu/Header.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 
 export class Header extends React.Component<any, any> {
     render() {
-        return <header>Catch of the Day</header>;
+        return (
+            <header className="menu-header">
+                <h1>Catch of the Day</h1>
+                <h3><span>Fresh seafood market</span></h3>
+            </header>
+        );
     }
 }
