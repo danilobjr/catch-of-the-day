ac81e07fdc4c454a56f10341f0da58c88aa28e01
Danilo Barros <danilobjr@gmail.com>
2016-05-21 17:20:37 -0300

    basic InventoryList component

--- src/components/inventory/Inventory.tsx
+++ src/components/inventory/Inventory.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Section, SectionHeader } from './../common';
+import { InventoryList } from './InventoryList';
 
 export class Inventory extends React.Component<any, any> {
     render() {
         return (
             <Section>
                 <SectionHeader>Inventory</SectionHeader>
+                <InventoryList />
+                {/*
+                <InventoryNewItem />
+                */}
             </Section>
         );
     }
---
+++ src/components/inventory/InventoryList.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export class InventoryList extends React.Component<any, any> {
+    render() {
+        return (
+            <ul>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+            </ul>
+        );
+    }
+}
