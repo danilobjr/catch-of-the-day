078db8eefda034d89f7bbee30eff0bb272d8e677
Danilo Barros <danilobjr@gmail.com>
2016-05-27 18:01:37 -0300

    passing external css classes

--- src/components/common/FlexBox.tsx
+++ src/components/common/FlexBox.tsx
@@ -1,3 +1,3 @@
 import * as React from 'react';
 
-export const FlexBox = (props: any) => <div className="flex-container">{props.children}</div>
+export const FlexBox = (props: any) => <div className={`flex-container ${props.className}`}>{props.children}</div>
