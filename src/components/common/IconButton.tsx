89531749d756cdab9c26354bd61c4727b5d2c707
Danilo Barros <danilobjr@gmail.com>
2016-05-27 18:48:47 -0300

    onClick event in IconButton component

--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -3,14 +3,19 @@ import { FlexBox } from './FlexBox';
 
 interface IProps {
     name: string;
+    onClick?: (event: Event) => void;
 }
 
 export class IconButton extends React.Component<IProps, any> {
     render() {
         return (
             <FlexBox className="icon-button">
-                <span className={`fa fa-${this.props.name}`}></span>
+                <span className={`fa fa-${this.props.name}`} onClick={this.onClick}></span>
             </FlexBox>
         );
+    }
+    
+    onClick = (event: any): void => {
+        this.props.onClick(event);
     }
 }
