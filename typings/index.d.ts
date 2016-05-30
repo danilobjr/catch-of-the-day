228fd3742ff169fadfc068c660c1991e7b1d7258
Danilo Barros <danilobjr@gmail.com>
2016-05-30 15:39:14 -0300

    installing pkg and typings for react-addons-css-transition-group

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/react-addons-css-transition-group/index.d.ts" />
+/// <reference path="globals/react-addons-transition-group/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
