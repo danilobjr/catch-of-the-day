9dbc95a0337296a9bbdd8f0157f940f38c95d97d
Danilo Barros <danilobjr@gmail.com>
2016-05-29 22:00:29 -0300

    es6-promise type definitions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
