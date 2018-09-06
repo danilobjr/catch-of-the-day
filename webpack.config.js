967003e65b0b86b5ca88dc577fafdd20d23507a9
Danilo Barros <danilobjr@gmail.com>
2018-09-05 21:02:59 -0300

    set webpack mode on development

--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,7 @@ const target = process.env.npm_lifecycle_event;
 
 if (target === 'dev' || !target) {
   const devConfig = {
-    // module: {
-    //     preLoaders: [
-    //         {
-    //             text: /\.js$/,
-    //             loader: 'source-map'
-    //         }
-    //     ]
-    // },
+    mode: 'development',
     devtool: 'eval-source-map',
     devServer: {
       contentBase: './build',
