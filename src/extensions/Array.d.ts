92bcc6a36f475531dcd8407f286dfd1c7df32c08
Danilo Barros <danilobjr@gmail.com>
2018-09-10 17:09:18 -0300

    remove unnecessary extensions

--- src/extensions/Array.d.ts
+++
@@ -1,5 +0,0 @@
-interface Array<T> {
-	find: (callback: (value: T, index: number, array: T[]) => boolean, thisArg?: any) => T;
-    findIndex: (callback: (value: T, index: number, array: T[]) => boolean, thisArg?: any) => number;
-    includes: (searchElement: T, fromIndex?: number) => boolean;
-}
