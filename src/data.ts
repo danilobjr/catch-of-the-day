2acce930e4c4046068e0763916dbd9b5e6dff059
Danilo Barros <danilobjr@gmail.com>
2018-09-11 16:58:28 -0300

    decrease description length

--- src/data.ts
+++ src/data.ts
@@ -6,7 +6,7 @@ export const data = {
     {
       id: generateId(),
       name: 'Pargo',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi cupiditate ducimus totam voluptate!',
+      description: 'Lorem ipsum dolor sit amet!',
       price: 25,
       imageUrl: 'http://www.bossame.com.br/wp-content/uploads/2013/07/Chez-L%C2%B4Ami-Martin_-Fil%C3%A9-de-Pargo-com-arroz-negro-e-lula-a-Dor%C3%A9_-cr%C3%A9dito-Roberto-Prince.jpg',
       available: true
